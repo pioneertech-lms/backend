c923a06309145543be9430aa6c4b2966e95af14c
Deep Bansode <drasticoder@gmail.com>
2023-10-10 02:15:21 +0530

    chore : user auth route

---
+++ routes/user/authRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  loginUser,  
+  userChangePassword,
+} from "../../controllers/user/authController.js";
+import { extractUserInfo } from "../../middleWares/accessAuth.js";
+
+const router = express.Router();
+
+router
+  .route("/login")
+  .post(extractUserInfo,loginUser);
+
+// router
+//   .route("/register")
+//   .post(registerUser)
+
+router
+  .route("/change-password")
+  .put(extractUserInfo,userChangePassword);
+
+export default router;
