0f1287295706a8598381cfddd2bdfbb32b41d6a1
Deep Bansode <drasticoder@gmail.com>
2023-10-17 02:34:35 +0530

    perf (backup) : set crone for auto backup to remote cluster

---
+++ utils/cron.js
@@ -0,0 +1,31 @@
+import cron from 'node-cron';
+import { exec }from  'child_process';
+
+cron.schedule('5 0 * * *', () => {
+  const backupDir = '../public/backup/';
+
+  const timestamp = new Date().toISOString().replace(/:/g, '-');
+
+  const backupFolder = `${backupDir}/${timestamp}`;
+
+  const backupCommand = `mongodump --uri=${process.env.MONGO_LOCAL_URI} --out=${backupFolder}`;
+  const restoreCommand = `mongorestore --uri=${process.env.MONGO_REMOTE_URI} ${backupFolder}`;
+
+  exec(backupCommand, (backupError, backupStdout, backupStderr) => {
+    if (backupError) {
+      console.error('Error during backup:', backupError);
+      return;
+    }
+
+    console.log('Database backup completed.');
+
+    exec(restoreCommand, (restoreError, restoreStdout, restoreStderr) => {
+      if (restoreError) {
+        console.error('Error during restore:', restoreError);
+        return;
+      }
+
+      console.log('Database restore to MongoDB Atlas completed.');
+    });
+  });
+});
