df76f079955f640270021598b3d7f1d7885e98e7
Deep Bansode <drasticoder@gmail.com>
2023-10-27 09:38:20 +0530

    chore: utils routes

---
+++ routes/user/utilsRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  uploadStatic,  
+} from "../../controllers/user/utilsController.js";
+import upload from "../../middleWares/uploads.js";
+// import { extractUserInfo } from "../../middleWares/accessAuth.js";
+
+const router = express.Router();
+
+router
+  .route("/uploads")
+  .post(upload,uploadStatic);
+
+
+export default router;
