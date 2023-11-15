bdb9af4617f130c43755aa5251136d9564f59b39
Deep Bansode <drasticoder@gmail.com>
2023-11-16 00:26:34 +0530

    Changed route from /test to /ping in app.js

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.use("/api/test/",testRoute);
 // app.use("/api/student",studentRoutes);
 
 // test route
-app.get("/test", (req,res)=>{
+app.get("/ping", (req,res)=>{
   // res.render("test");
   res.status(200).send("Server is up & running...")
 })
