3da42a2a76780c1ffb4206b4cb602ada44a6c7ff
Deep Bansode <drasticoder@gmail.com>
2023-11-16 11:38:07 +0530

    Commented out unique constraint for slug field in User model

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const schema = new mongoose.Schema(
     slug: {
       type: String,
       // required: true,
-      unique: true,
+      // unique: true,
     },
     email: {
       type: String,
