34f6a58a90eb15e9ebc3f806347ab0319284c2c3
Pranav Karawale <pranav@karawale.in>
2023-11-22 11:08:01 +0530

    Fix upload code

--- middleWares/uploads.js
+++ middleWares/uploads.js
@@ -7,6 +7,7 @@ export const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'assets',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read', // Set the ACL (Access Control List) as needed
     key: function (req, file, cb) {
       const objectName = `assets/${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
