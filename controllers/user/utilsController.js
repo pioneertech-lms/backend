a0bf685c75e4cb632f3b82b7e773f2483811b310
Pranav Karawale <pranav@karawale.in>
2024-01-02 15:10:35 +0530

    Modify materials api

--- controllers/user/utilsController.js
+++ controllers/user/utilsController.js
@@ -1,7 +1,7 @@
 import { bucketName, s3 } from "../../config/storageObject.js";
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import { catchAsyncError } from "../../middleWares/catchAsyncError.js";
-import {Question} from "../../models/Question.js";
+import { Question } from "../../models/Question.js";
 
 export const uploadStatic = catchAsyncError(async (req, res, next) => {
   const files = req.files;
@@ -22,13 +22,51 @@ export const listMaterial = catchAsyncError(async (req, res, next) => {
       Prefix: "material/"
     }));
 
-    const keys = (data.Contents ?? []).map(object => object.Key.replace(/^(material\/)/, ""));
-    return res.status(200).json(keys)
+    const contents = (data.Contents ?? []);
+
+    // Process Question Banks
+    const questionBanks = contents
+      .filter(object => object.Key.startsWith("material/question-bank/"))
+      .reduce((acc, object) => {
+        const path = object.Key.replace("material/question-bank/", "");
+        const [examName, yearWithExtension] = path.split('/');
+        if (!acc[examName]) {
+          acc[examName] = {};
+        }
+        const year = yearWithExtension.replace('.pdf', '');
+        acc[examName][year] = new URL("assets/" + object.Key, process.env.BACKEND_URL);
+        return acc;
+      }, {});
+
+    // Process Formulae
+    const formulae = contents
+      .filter(object => object.Key.startsWith("material/formulae/"))
+      .reduce((acc, object) => {
+        const path = object.Key.replace("material/formulae/", "");
+        const [examName, subject, grade, chapterWithExtension] = path.split('/');
+        if (!acc[examName]) {
+          acc[examName] = {};
+        }
+        if (!acc[examName][subject]) {
+          acc[examName][subject] = {};
+        }
+        if (!acc[examName][subject][grade]) {
+          acc[examName][subject][grade] = {};
+        }
+        const chapter = chapterWithExtension.replace('.pdf', '');
+        acc[examName][subject][grade][chapter] = new URL("assets/" + object.Key, process.env.BACKEND_URL);
+        return acc;
+      }, {});
+
+    return res.status(200).json({
+      questionBanks,
+      formulae
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Failed to fetch material" })
   }
-})
+});
 
 export const getQueCountByTeacherId = catchAsyncError(async (req, res, next) => {
   const { id } = req.params;
